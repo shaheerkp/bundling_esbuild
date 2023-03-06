00dd1fa92254ebb27439c2fe2a6fbc70bd7c8682
shaheerkp <kpshaheer123@gmail.com>
2023-03-06 21:50:52 +0530

    code editor auto formatting

--- src/components/EsBuild.tsx
+++ src/components/EsBuild.tsx
@@ -3,6 +3,7 @@ import * as esbuild from 'esbuild-wasm'
 import { unpkgPathPlugin } from '../plugins/unpkg-path-plugin';
 import { fetchPlugin } from '../plugins/fetch-plugin';
 import CodeEditor from './code-editor';
+import "bulmaswatch/superhero/bulmaswatch.min.css";
 
 
 const EsBuild = () => {
--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,5 +1,12 @@
 import Editor,{OnMount} from "@monaco-editor/react"
 import { useRef } from "react";
+import prettier from "prettier"
+import parser from "prettier/parser-babel" 
+import './code-editor.css' 
+import codeShift from "jscodeshift"
+import Highlighter from "monaco-jsx-highlighter"
+
+
 
 
 interface CodeEditorProps{
@@ -9,10 +16,32 @@ interface CodeEditorProps{
 
 const CodeEditor:React.FC<CodeEditorProps>=({initialValue,onChange})=>{
 
+  
+
     const editorRef = useRef<any>(null);
 
-    const handleMount:OnMount=(editor,monaco)=> {
+    const handleMount:OnMount=async(editor,monaco)=> {
+      const { default: traverse } = await import("@babel/traverse");
+const { parse } = await import("@babel/parser");
+
       editorRef.current = editor;
+      // const highlighter=new Highlighter(
+      //   //@ts-ignore
+      //   monaco,
+      //   parse,
+      //   traverse,
+      //   editor
+        
+       
+        
+      // );
+      // highlighter.highLighterOnDidChangeModelContent(
+      //   ()=>{},
+      //   ()=>{},
+      //   undefined,
+      //   ()=>{}
+      // )
+     
     }
 
     function showValue() {
@@ -20,9 +49,16 @@ const CodeEditor:React.FC<CodeEditorProps>=({initialValue,onChange})=>{
             onChange(editorRef.current.getValue()) 
         }
       }
+      const onFormatClick=()=>{
+         const unformatted=editorRef.current.getValue()
+         const formatted=prettier.format(unformatted,{parser:'babel',plugins:[parser],useTabs:false,semi:true,singleQuote:true}).replace(/\n$/,"");
+         editorRef.current.setValue(formatted)
+      }
     
 
-return <Editor
+return <div className="editor-wrapper">
+<button className="button button-format is-primary is-small" onClick={onFormatClick}>Format</button>
+<Editor
 onChange={showValue}
 value={initialValue}
 onMount={handleMount}
@@ -40,6 +76,7 @@ onMount={handleMount}
     automaticLayout:true,
  }}
  />
+</div>
 
 
 }
